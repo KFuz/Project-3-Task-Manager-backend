a0e1e8507959f6e80557f79a9aa1ba690f1ec13c
doodybh <doodybhr38@gmail.com>
2026-04-14 19:37:41 +0300

    get routes created for one task and all tasks (read)

--- controllers/task.routes.js
+++ controllers/task.routes.js
@@ -17,4 +17,33 @@ router.post("/", async (req, res) => {
   }
 });
 
+// GET /tasks
+router.get("/", async (req, res) => {
+  try {
+    const tasks = await Task.find({ user: req.user._id }).sort({
+      createdAt: -1,
+    });
+    res.status(200).json({ tasks });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ err: err.message });
+  }
+});
+
+// GET /tasks/:id
+router.get("/:id", async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+
+    if (!task) {
+      return res.status(404).json({ err: "Task not found" });
+    }
+
+    res.status(200).json({ task });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ err: err.message });
+  }
+});
+
 module.exports = router;
